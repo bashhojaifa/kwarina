2ab8dcef6dd9b173e7fdb24e5c194934f95e1bd2
#unclexo <abujobaer@gmail.com>
2020-06-24 23:08:32 +0600

    Checks whether or not the user has liked

--- app/Http/Controllers/Auth/LikeController.php
+++ app/Http/Controllers/Auth/LikeController.php
@@ -2,44 +2,21 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Like;
 use App\Post;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class LikeController extends Controller
 {
     public function postLikePost(Request $request)
     {
-        $post_id = $request['postId'];
-        $is_like = $request['isLike'] === 'true';
-        $update = false;
-        $post = Post::find($post_id);
-        if (!$post) {
-            return null;
-        }
-        $user = Auth::user();
-        $like = $user->likes()->where('post_id', $post_id)->first();
-        if ($like) {
-            $already_like = $like->like;
-            $update = true;
-            if ($already_like == $is_like) {
-                $like->delete();
-                return null;
-            }
-        } else {
-            $like = new Like();
-        }
-        $like->like = $is_like;
-        $like->user_id = $user->id;
-        $like->post_id = $post->id;
-        if ($update) {
-            $like->update();
-        } else {
-            $like->save();
-        }
-        return null;
+        $postId = $request->input('postId');
+        $userId = $request->input('userId');
+
+        $hasLiked = DB::table('likes')
+            ->where(['user_id' => $userId, 'post_id' => $postId, 'like' => '1'])
+            ->pluck('like');
     }
 
 }
